3352bfcd392a1cec532d88ee8461a42e52b052c8
Nishant <31662663+nishbafna@users.noreply.github.com>
2018-03-13 23:41:45 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
-# Baemax-master
+# Baemax
+
+Baemax is your personal healthcare assistant. Inspired from the fictional character “Baymax”, our conversational bot does quite the same.
+If you’re feeling blue, Baemax will engage you in a conversation, ask about your symptoms, correspondingly suggest some home remedies for it.
+Let it be a common cold or a headache or stomachache, Baemax will give you simple tips/ hacks (strictly non-medicinal) to make you feel alright.
+Say you’re feeling sad, Baemax will prompt you with a perfect playlist that can ease you up and make you feel better (it has great taste in music) or share something interesting and entertaining with you.
+For women on periods, say you’re craving for something sweet, Baemax will find you healthy substitutes for chocolates and will also suggest some home remedies for dealing with cramps.
+Depression has become an emerging problem and must be stemmed at roots. Baemax can be a part of counsel and engage them with games and warmth.
+Trust me this is just the beginning of endless series of applications of Baemax.
+About time you found a real Bae. Don't you think?
+
+Technical aspects - 
+
+What you're seeing is a simple Web application made using HTML5, JavaScript and jQuery.
+Initially, the user can communicate by typing or give a voice  command.
+Using WebKit Speech Synthesis, the program converts voice to text and sends it to the api. This text is run through an NLP Parser code written in python which breaks the sentences into phrases. Matches the input and passes the subsequent output in the form of a JSON file.
+This JSON file reaches our Application where it gets converted back to Voice.
+
+Demo - http://gofornaman.co/baemax
---
+++ test.php
@@ -0,0 +1,607 @@
+<?php
+error_reporting(E_ALL);
+$servername = "127.0.0.1";
+$username = "root";
+$password = "";
+$dbname = "data";
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+
+$value = $_POST['field1'];
+$value2 = $_POST['field2'];
+$sql = "INSERT INTO tyh (activity, happylev)
+VALUES ('$value', '$value2')";
+
+mysqli_close($conn);
+?>
+<html>
+<head>
+  <title>Friday</title>
+  <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+  <script src="http://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
+  <script type="text/javascript">
+    var accessToken = "794015458e22456d944550441ba333b4",
+      baseUrl = "https://api.api.ai/v1/",
+      $speechInput,
+      $recBtn,
+      recognition,
+      messageRecording = "Uhmm. I hear you!",
+      messageCouldntHear = "God, your voice is horrible. Could you say that again?",
+      messageInternalError = "Oh no, there has been an internal server error",
+      messageSorry = "Come on man. I don't know. I'm not Jarvis!";
+
+
+    $(document).ready(function() {
+      $speechInput = $("#speech");
+      $recBtn = $("#rec");
+
+      $speechInput.keypress(function(event) {
+        if (event.which == 13) {
+          event.preventDefault();
+          send();
+        }
+      });
+      $recBtn.on("click", function(event) {
+        switchRecognition();
+      });
+      $(".debug__btn").on("click", function() {
+        $(this).next().toggleClass("is-active");
+        return false;
+      });
+    });
+
+    function startRecognition() {
+      recognition = new webkitSpeechRecognition();
+      recognition.continuous = false;
+          recognition.interimResults = false;
+
+      recognition.onstart = function(event) {
+        respond(messageRecording);
+        updateRec();
+      };
+      recognition.onresult = function(event) {
+        recognition.onend = null;
+        
+        var text = "";
+          for (var i = event.resultIndex; i < event.results.length; ++i) {
+            text += event.results[i][0].transcript;
+          }
+          setInput(text);
+        stopRecognition();
+      };
+      recognition.onend = function() {
+        respond(messageCouldntHear);
+        stopRecognition();
+      };
+      recognition.lang = "en-US";
+      recognition.start();
+    }
+  
+    function stopRecognition() {
+      if (recognition) {
+        recognition.stop();
+        recognition = null;
+      }
+      updateRec();
+    }
+
+    function switchRecognition() {
+      if (recognition) {
+        stopRecognition();
+      } else {
+        startRecognition();
+      }
+    }
+
+    function setInput(text) {
+      $speechInput.val(text);
+      send();
+    }
+
+    function updateRec() {
+      $recBtn.text(recognition ? "Stop" : "");
+    }
+
+    function send() {
+      var text = $speechInput.val();
+      $.ajax({
+        type: "POST",
+        url: baseUrl + "query",
+        contentType: "application/json; charset=utf-8",
+        dataType: "json",
+        headers: {
+          "Authorization": "Bearer " + accessToken
+        },
+        data: JSON.stringify({query: text, lang: "en", sessionId: "yaydevdiner"}),
+
+        success: function(data) {
+          prepareResponse(data);
+        },
+        error: function() {
+          respond(messageInternalError);
+        }
+      });
+    }
+
+    function prepareResponse(val) {``
+      var debugJSON = JSON.stringify(val, undefined, 2),
+        spokenResponse = val.result.speech;
+
+      respond(spokenResponse);
+      debugRespond(debugJSON);
+    }
+
+    function debugRespond(val) {
+      $("#response").text(val);
+    }
+
+    function respond(val) {
+      if (val == "") {
+        val = messageSorry;
+      }
+
+      if (val !== messageRecording) {
+        var msg = new SpeechSynthesisUtterance();
+        msg.voiceURI = "native";
+        msg.text = val;
+        msg.lang = "en-US";
+        window.speechSynthesis.speak(msg);
+      }
+
+
+      $("#spokenResponse").addClass("is-active").find(".spoken-response__text").html(val);
+    }
+  </script>
+  <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js" type="text/javascript"></script>
+<script src="http://ajax.googleapis.com/ajax/libs/jqueryui/1.8.16/jquery-ui.min.js" type="text/javascript"></script>
+<meta charset="utf-8">
+<script type="text/javascript">
+    var stub_showing = false;
+ 
+    function woahbar_show() { 
+        if(stub_showing) {
+          $('.woahbar-stub').slideUp('fast', function() {
+            $('.woahbar').show('bounce', { times:3, distance:15 }, 100); 
+            $('body').animate({"marginTop": "2.4em"}, 250);
+          }); 
+        }
+        else {
+          $('.woahbar').show('bounce', { times:3, distance:15 }, 100); 
+          $('body').animate({"marginTop": "2.4em"}, 250);
+        }
+    }
+ 
+    function woahbar_hide() { 
+        $('.woahbar').slideUp('fast', function() {
+          $('.woahbar-stub').show('bounce', { times:3, distance:15 }, 100);  
+          stub_showing = true;
+        }); 
+ 
+        if( $(window).width() > 1024 ) {
+          $('body').animate({"marginTop": "0px"}, 250); // if width greater than 1024 pull up the body
+        }
+    }
+ 
+    $().ready(function() {
+        window.setTimeout(function() {
+        woahbar_show();
+     }, 4000);
+    });
+</script>
+  <style type="text/css">
+    html {
+      box-sizing: border-box;
+    }
+    *, *:before, *:after {
+      box-sizing: inherit;
+    }
+    body {
+      background-color: #000000;
+	   
+      background-image: url("d.gif");
+	background-attachment: fixed;
+    background-position: 570px 50px;
+    background-repeat: no-repeat;
+<!--background-size: cover;-->
+   
+	  
+      font-family: "Titillium Web", Arial, sans-serif;
+      font-size: 20px;
+      margin: 0;
+    }
+    .container {
+      position: fixed;
+      top: 45%;
+      right:55%;
+		left:17%;
+      -webkit-transform: translate(-50%, -50%);
+    }
+	 .container1 {
+      position: fixed;
+      top: 30%;
+      left: 80%;
+	 
+      -webkit-transform: translate(-50%, -50%);
+    }
+	
+    input {
+      background-color: #000000;
+      border: 1px solid #000000;
+      color: #FFFFFF;
+      font-family: "Oswald";
+      font-size: 20px;
+      line-height: 43px;
+      padding: 0 0.75em;
+      width: 350px;
+      -webkit-transition: all 0.35s ease-in;
+    }
+    textarea {
+      background-color: #000000;
+      border: 1px solid #47ff14;
+      color: #000000;
+      padding: 0.5em;
+      width: 100%;
+      -webkit-transition: all 0.35s ease-in;
+    }
+    input:active, input:focus, textarea:active, textarea:focus {
+      outline: 1px solid #000000;
+    }
+    .btn {
+      background-color: #000000;
+	  background-position:center;
+	  background-repeat:no-repeat;
+      border: 0px solid #000000;
+      color: #549EAF;
+      cursor: pointer;
+      //padding-top: 200px;
+	  width: 40px;
+	  height: 50px;
+	  top:20%;
+	  left:70%;
+	 
+    }
+    .btn:hover {
+      background-color: #1888A9;
+      color: #183035;
+    }
+	
+    .debug {
+      bottom: 0;
+      position: fixed;
+      right: 0;
+    }
+    .debug__content {
+      font-size: 14px;
+      max-height: 0;
+      overflow: hidden;
+      -webkit-transition: all 0.35s ease-in;
+    }
+    .debug__content.is-active {
+      display: block;
+      max-height: 500px;
+    }
+    .debug__btn {
+      width: 50%;
+    }
+    .spoken-response {
+      max-height: 0;
+      overflow: hidden;
+      -webkit-transition: all 0.35s ease-in;
+    }
+    .spoken-response.is-active {
+      max-height: 2000px;
+    }
+    .spoken-response__text {
+	top:20%;
+      background-color: #000000;
+      color: #E1E1E1;
+	   font-family: "Oswald";
+		width:350px;
+      padding: 1em;
+	  font-size: 20px;
+    }
+	  .a{
+		  padding-top: 200px;
+		  align-content: center;
+		  
+	  }
+	  .input_align{
+		  padding-left: 100px;
+	  }
+	  .mac{
+		  padding-left: 200px;
+	  }
+.container3 {
+	width: 500px;
+	margin: 0 auto;
+	padding-top: 120px;
+}
+.button_aligner{
+		  float: left;
+	padding-right: 200px;
+	  } 
+	  .try{
+		  float: none;
+		  padding-left:140px;
+		  padding-top: 100px;
+		  
+	  }
+	  
+.buttonee{
+	
+  background-color: #000000;
+    border: none;
+    color: white;
+    padding: 10px 22px;
+    text-align: center;
+    text-decoration: none;
+    display: inline-block;
+    font-size: 16px;
+    margin: 4px 2px;
+    -webkit-transition-duration: 0.4s; /* Safari */
+    transition-duration: 0.4s;
+    cursor: pointer;
+    
+}
+
+.buttonee:hover {
+	 background-color: #000000;
+    color: white;
+	margin-top: 20px;
+}	
+.buttonee1{
+	 background-color: #000000;
+    border: none;
+    color: white;
+     padding: 10px 22px;
+    text-align: center;
+    text-decoration: none;
+    display: inline-block;
+    font-size: 16px;
+    margin: 4px 2px;
+    -webkit-transition-duration: 0.4s; /* Safari */
+    transition-duration: 0.4s;
+    cursor: pointer;
+     -webkit-transition: all 1s ease;
+    -moz-transition: all 1s ease;
+    -o-transition: all 1s ease;
+}
+
+.buttonee1:hover {
+	 background-color: #000000;
+    color: white;
+	margin-top: 5px;
+}	
+.buttonee2{
+	 background-color: #000000;
+    border: none;
+    color: white;
+    padding: 10px 22px;
+    text-align: center;
+    text-decoration: none;
+    display: inline-block;
+    font-size: 16px;
+    margin: 4px 2px;
+    -webkit-transition-duration: 0.4s; /* Safari */
+    transition-duration: 0.4s;
+    cursor: pointer;
+     -webkit-transition: all 1s ease;
+    -moz-transition: all 1s ease;
+    -o-transition: all 1s ease;
+}
+
+.buttonee2:hover {
+ background-color: #000000;
+    color: white;
+	margin-top: 5px;
+}	
+	  .voice{
+		  padding-left: 650px;
+		  padding-top: 270px;
+	  }
+	  .woahbar
+{
+  position: fixed;
+	
+  top: 45%;
+  left: 76%;
+  width: 400px;
+  height: 250px;
+  z-index: 100;
+  padding: 5px 0 5px 0;
+  text-align: center;
+  font-size: 110%;
+  color: #fff;
+  background-color: #000000;  /* << set custom bar color here */
+  font-family: Georgia,Times New Roman,Times,serif;
+}
+ 
+ 
+.woahbar span
+{
+  float: left;
+  width: 95%;
+  text-align: center;
+  padding-top: 2px;
+}
+ 
+
+.woahbar-down-arrow:hover {
+  background-color:#625EBD; /* << set custom hover bar color here */
+}
+ 
+.woahbar-up-arrow: {
+  background-color:#FFFFFF;
+}
+ 
+
+.close-notify
+{
+  float: right;
+  margin-right: 22px;
+  color: #fff;
+  width: 17px;
+  height: 19px;
+  text-decoration: none;
+  background-color: #625EBD; /* << set custom bar color here */
+  cursor:pointer;
+}
+ 
+.show-notify
+{
+  
+  border: 3px solid #fff;
+  box-shadow: 0 0 5px rgba(0,0,0,0.35);
+ 
+  float: right;
+  
+  color: #fff;
+  width: 35px;
+  height: 33px;
+  text-decoration: none;
+  background-color:#2731A4; /* << set custom bar color here */
+  cursor:pointer;
+}
+	.a{
+		padding-top: 10px;
+		width: 300px;
+		 color:white;
+	}
+	  .b{
+		 padding-left: 50px;
+	  }
+	  .bubby{
+		  color: white;
+		  background-color: black;
+		  width: 20px;
+	  }
+	  input.e{
+		  background-color: #8B8B8B;
+	  }
+	  .form-style-6{
+    font: 95% Arial, Helvetica, sans-serif;
+    max-width: 400px;
+    margin: 10px auto;
+    padding: 16px;
+    background-color: #222121;
+}
+.form-style-6 h1{
+    background-color: #090808;
+    padding: 20px 0;
+    font-size: 140%;
+    font-weight: 300;
+    text-align: center;
+    color: #fff;
+    margin: -16px -16px 16px -16px;
+}
+.form-style-6 input[type="text"],
+.form-style-6 input[type="date"],
+.form-style-6 input[type="datetime"],
+.form-style-6 input[type="email"],
+.form-style-6 input[type="number"],
+.form-style-6 input[type="search"],
+.form-style-6 input[type="time"],
+.form-style-6 input[type="url"],
+.form-style-6 textarea,
+.form-style-6 select 
+{
+    -webkit-transition: all 0.30s ease-in-out;
+    -moz-transition: all 0.30s ease-in-out;
+    -ms-transition: all 0.30s ease-in-out;
+    -o-transition: all 0.30s ease-in-out;
+    outline: none;
+    box-sizing: border-box;
+    -webkit-box-sizing: border-box;
+    -moz-box-sizing: border-box;
+    width: 100%;
+    background: #222121;
+    margin-bottom: 4%;
+    border: 1px solid #ccc;
+    padding: 3%;
+    color: #DBDBDB;
+    font: 95% Arial, Helvetica, sans-serif;
+}
+.form-style-6 input[type="text"]:focus,
+.form-style-6 input[type="date"]:focus,
+.form-style-6 input[type="datetime"]:focus,
+.form-style-6 input[type="email"]:focus,
+.form-style-6 input[type="number"]:focus,
+.form-style-6 input[type="search"]:focus,
+.form-style-6 input[type="time"]:focus,
+.form-style-6 input[type="url"]:focus,
+.form-style-6 textarea:focus,
+.form-style-6 select:focus
+{
+    box-shadow: 0 0 5px #43D1AF;
+    padding: 3%;
+    border: 1px solid #43D1AF;
+}
+
+.form-style-6 input[type="submit"],
+.form-style-6 input[type="button"]{
+    box-sizing: border-box;
+    -webkit-box-sizing: border-box;
+    -moz-box-sizing: border-box;
+    width: 100%;
+    padding: 3%;
+	padding-top: 5px;
+    background: #090808;
+       border-top-style: none;
+    border-right-style: none;
+    border-left-style: none;    
+    color: #fff;
+}
+.form-style-6 input[type="submit"]:hover,
+.form-style-6 input[type="button"]:hover{
+    background: #222121;
+}
+
+  </style>
+  
+  <link rel="icon" href="1images.jpg" type="image/gif" >
+</head>
+<body>
+ <div class="container">
+    <input id="speech" type="text"> <br> <br>
+</div>
+<div class="voice">
+	<button id="rec" class="btn"></button>
+	</div>
+	 <div class="container1">
+   <div class="a">
+    <div id="spokenResponse" class="spoken-response">
+      <div class="spoken-response__text"></div>
+    </div>
+		 </div>
+  </div>
+  
+    <div class="debug__content">
+      <textarea id="response" cols="40" rows="20"></textarea>
+    </div>
+ <div class="try">
+    <div class="container3">
+		<button class="buttonee"><a href="https://l.messenger.com/l.php?u=https%3A%2F%2Fbaemax2.slack.com%2Fmessages%2F%40baemax%2F&h=ATMTZufdo8RLAOrwoyd7wtx2LwXsdIpAVM2s3QU-Dw2_L3Pe-0YTKT-31FpkwkW6dv_53hkKCp7Bv-HdHM4uFv3UQFldcxg5zefRC6gf4UqwXVt21-iR4hrcoUKVZ4vKaHZn6Eu-"><img src="slack.png" width="60" height="60"></a></button>
+   
+    <button class="buttonee"><a href="https://www.messenger.com/t/434955823529159"><img src="mess.png" width="50" height="50"></a></button>
+    <button class="buttonee"><a href="https://l.messenger.com/l.php?u=https%3A%2F%2Ftwitter.com%2Fgofornaman&h=ATMTZufdo8RLAOrwoyd7wtx2LwXsdIpAVM2s3QU-Dw2_L3Pe-0YTKT-31FpkwkW6dv_53hkKCp7Bv-HdHM4uFv3UQFldcxg5zefRC6gf4UqwXVt21-iR4hrcoUKVZ4vKaHZn6Eu-"><img src="twitter.png" width="50" height="50"></a></button>
+  </div>
+</div>
+ <div class="woahbar" style="display:none">
+   <span>
+<div class="form-style-6">
+<h1>OK Listen!</h1>
+<form>
+<input type="text" name="field1" placeholder="Watcha Doin?" />
+<input type="text" name="field2" placeholder="How happy are you? Rate it(Max:10)" />
+<input type="submit" value="Send" />
+<input type="submit" value="View Weekly Results" />
+</form>
+</div>
+    </span>
+    <a class="close-notify" onclick="woahbar_hide();"><img class="woahbar-up-arrow" src="x.png" height="10px" width="10px"></a>
+</div>
+  <link href="https://fonts.googleapis.com/css?family=Titillium+Web:200" rel="stylesheet" type="text/css">
+</body>
+</html>
